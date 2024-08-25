f6979f9cc03654824c582efcb581c4e975013764
omarknows <omarknows98@gmail.com>
2024-08-25 21:29:13 +0300

    Added link to documentation in task 5

--- src/tasks/TaskFive/TaskFive.tsx
+++ src/tasks/TaskFive/TaskFive.tsx
@@ -3,13 +3,20 @@ import Container from '@components/Container';
 import { Profiler, useState } from 'react';
 import Optimized from './components/optimized/Optimized';
 import Unoptimized from './components/unoptimized/Unoptimized';
+import { Link } from 'react-router-dom';
 
 const TaskFive = () => {
 	const [rerender, setRerender] = useState(true);
 	return (
 		<Container title='Task Five: Optimize a React Component'>
-			<Button onClick={() => setRerender((prev) => !prev)}>Rerender</Button>
-			<div className='flex gap-10 mt-5'>
+			<Link to='https://github.com/OmarKnows/ecom-payments-assessment'>
+				Click Here to navigate to the documentation where everything regarding this task is explained under the "Task 5"
+				section
+			</Link>
+			<Button className='my-5' onClick={() => setRerender((prev) => !prev)}>
+				Rerender
+			</Button>
+			<div className='flex gap-10'>
 				<Profiler
 					id='Optimized'
 					onRender={(id, phase, actualDuration, baseDuration, startTime, commitTime) =>
