50e551dcdf9905302fec55d6129c0970225f1a11
OmarKnows <omarknows98@gmail.com>
2024-08-25 16:44:41 +0300

    created use fetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+type TFetchState<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+};
+
+const useFetch = <T>(url: string): TFetchState<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("There was an error fetching the data");
+        }
+        const result: T = await response.json();
+        setData(result);
+      } catch (error) {
+        setError((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
