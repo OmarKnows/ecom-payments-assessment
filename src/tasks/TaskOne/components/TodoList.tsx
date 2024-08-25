118951bb47f7efabe12bc79a4a73dc6c45e8a457
OmarKnows <omarknows98@gmail.com>
2024-08-25 16:14:33 +0300

    added empty state

--- src/tasks/TaskOne/components/TodoList.tsx
+++ src/tasks/TaskOne/components/TodoList.tsx
@@ -10,36 +10,42 @@ interface TodoListProps {
 
 const TodoList: FC<TodoListProps> = ({ todos, toggleTodo, deleteTodo }) => {
   return (
-    <table className="w-[300px] border-collapse text-center">
-      <thead>
-        <tr>
-          <th className="border p-2">Completed</th>
-          <th className="border p-2">Title</th>
-          <th className="border p-2">Description</th>
-          <th className="border p-2">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {todos.map((todo) => (
-          <tr key={todo.id}>
-            <td className="border p-2">
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleTodo(todo.id)}
-              />
-            </td>
-            <td className="border p-2">{todo.title}</td>
-            <td className="border p-2">{todo.description}</td>
-            <td className="border p-2">
-              <Button variant="red" onClick={() => deleteTodo(todo.id)}>
-                Delete
-              </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="w-[300px] border-collapse text-center">
+      {todos.length === 0 ? (
+        <p className="p-4 text-gray-500">No todos available</p>
+      ) : (
+        <table className="w-full border-collapse text-center">
+          <thead>
+            <tr>
+              <th className="border p-2">Completed</th>
+              <th className="border p-2">Title</th>
+              <th className="border p-2">Description</th>
+              <th className="border p-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td className="border p-2">
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => toggleTodo(todo.id)}
+                  />
+                </td>
+                <td className="border p-2">{todo.title}</td>
+                <td className="border p-2">{todo.description}</td>
+                <td className="border p-2">
+                  <Button variant="red" onClick={() => deleteTodo(todo.id)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
