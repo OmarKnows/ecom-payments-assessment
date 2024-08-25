3a88e20aa960e6e2e2bf3bbaa10f35aecc991dc2
OmarKnows <omarknows98@gmail.com>
2024-08-25 15:29:12 +0300

    used container in homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,31 @@
 import Button from "@components/Button";
-import { ROUTES } from "@constants/routes";
+import Container from "@components/Container";
+import { ROUTES } from "@/constants/routes";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center h-[100vh] gap-5">
-      <Button variant="blue" onClick={() => navigate(ROUTES.TASK_ONE)}>
-        Task One
-      </Button>
-      <Button variant="green" onClick={() => navigate(ROUTES.TASK_TWO)}>
-        Task Two
-      </Button>
-      <Button variant="orange" onClick={() => navigate(ROUTES.TASK_THREE)}>
-        Task Three
-      </Button>
-      <Button variant="red" onClick={() => navigate(ROUTES.TASK_FOUR)}>
-        Task Four
-      </Button>
-      <Button variant="yellow" onClick={() => navigate(ROUTES.TASK_FIVE)}>
-        Task Five
-      </Button>
-    </div>
+    <Container>
+      <div className="flex gap-5">
+        <Button variant="blue" onClick={() => navigate(ROUTES.TASK_ONE)}>
+          Task One
+        </Button>
+        <Button variant="green" onClick={() => navigate(ROUTES.TASK_TWO)}>
+          Task Two
+        </Button>
+        <Button variant="orange" onClick={() => navigate(ROUTES.TASK_THREE)}>
+          Task Three
+        </Button>
+        <Button variant="red" onClick={() => navigate(ROUTES.TASK_FOUR)}>
+          Task Four
+        </Button>
+        <Button variant="yellow" onClick={() => navigate(ROUTES.TASK_FIVE)}>
+          Task Five
+        </Button>
+      </div>
+    </Container>
   );
 };
 
