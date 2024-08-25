31535df6af23ec46eaa56ecf3a27a65d998aadf3
OmarKnows <omarknows98@gmail.com>
2024-08-25 14:50:25 +0300

    setup router

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,27 +1,45 @@
-import App from '@/App';
-import { ROUTES } from '@constants/routes';
-import DummyDetailsPage from '@modules/DummyModule/dummy-details-page';
-import DummyListPage from '@modules/DummyModule/dummy-list-page';
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import App from "@/App";
+import TaskFive from "@/tasks/TaskFive";
+import TaskFour from "@/tasks/TaskFour";
+import TaskOne from "@/tasks/TaskOne";
+import TaskThree from "@/tasks/TaskThree";
+import TaskTwo from "@/tasks/TaskTwo";
+import { ROUTES } from "@constants/routes";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
-			{
-				path: ROUTES.DUMMY,
-				children: [
-					{
-						index: true,
-						element: <DummyListPage />,
-					},
-					{
-						path: ':id',
-						element: <DummyDetailsPage />,
-					},
-				],
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to={ROUTES.TASK_ONE} replace />,
+      },
+      {
+        path: ROUTES.TASK_ONE,
+        element: <TaskOne />,
+      },
+      {
+        path: ROUTES.TASK_TWO,
+        element: <TaskTwo />,
+      },
+      {
+        path: ROUTES.TASK_THREE,
+        element: <TaskThree />,
+      },
+      {
+        path: ROUTES.TASK_FOUR,
+        element: <TaskFour />,
+      },
+      {
+        path: ROUTES.TASK_FIVE,
+        element: <TaskFive />,
+      },
+      {
+        path: "*",
+        element: <Navigate to={ROUTES.TASK_ONE} replace />,
+      },
+    ],
+  },
 ]);
