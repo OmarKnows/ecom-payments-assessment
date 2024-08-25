c0c03014b03e41616047a56199f7addd6812417b
OmarKnows <omarknows98@gmail.com>
2024-08-25 16:45:10 +0300

    completed task two

--- src/tasks/TaskOne/components/TodoList.tsx
+++ src/tasks/TaskOne/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { ITodo } from "@/shared/types/Todo";
 import Button from "@components/Button";
+import Table from "@components/Table";
 import { FC } from "react";
 
 interface TodoListProps {
@@ -11,40 +12,29 @@ interface TodoListProps {
 const TodoList: FC<TodoListProps> = ({ todos, toggleTodo, deleteTodo }) => {
   return (
     <div className="w-[400px] border-collapse text-center">
-      {todos.length === 0 ? (
-        <p className="p-4 text-gray-500">No todos available</p>
-      ) : (
-        <table className="w-full border-collapse text-center">
-          <thead>
-            <tr>
-              <th className="border p-2">Completed</th>
-              <th className="border p-2">Title</th>
-              <th className="border p-2">Description</th>
-              <th className="border p-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map((todo) => (
-              <tr key={todo.id}>
-                <td className="border p-2">
-                  <input
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={() => toggleTodo(todo.id)}
-                  />
-                </td>
-                <td className="border p-2">{todo.title}</td>
-                <td className="border p-2">{todo.description}</td>
-                <td className="border p-2">
-                  <Button variant="red" onClick={() => deleteTodo(todo.id)}>
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      <Table<ITodo>
+        headers={["Completed", "Title", "Description", "Actions"]}
+        data={todos}
+        renderRow={(todo) => (
+          <>
+            <td className="border p-2">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleTodo(todo.id)}
+              />
+            </td>
+            <td className="border p-2">{todo.title}</td>
+            <td className="border p-2">{todo.description}</td>
+            <td className="border p-2">
+              <Button variant="red" onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </Button>
+            </td>
+          </>
+        )}
+        emptyMessage="No todos available"
+      />
     </div>
   );
 };
--- src/tasks/TaskTwo.tsx
+++ src/tasks/TaskTwo.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import useFetch from "@/hooks/useFetch";
+import Container from "@components/Container";
+import Table from "@components/Table";
+import { TailSpin } from "react-loader-spinner";
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
 
 const TaskTwo = () => {
-  return <div>TaskTwo</div>;
+  const { data, loading, error } = useFetch<IUser[]>(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return (
+    <Container title="Task Two: Implement a Custom Hook">
+      {loading ? (
+        <TailSpin wrapperClass="w-full flex justify-center items-center" />
+      ) : error ? (
+        <p className="p-4 text-red-500 text-xl font-semibold">{error}</p>
+      ) : (
+        <div className="w-[500px]">
+          <Table<IUser>
+            headers={["ID", "Name", "Email"]}
+            data={data || []}
+            renderRow={(user) => (
+              <>
+                <td className="border p-2">{user?.id}</td>
+                <td className="border p-2">{user?.name}</td>
+                <td className="border p-2">{user?.email}</td>
+              </>
+            )}
+            emptyMessage="No users available"
+          />
+        </div>
+      )}
+    </Container>
+  );
 };
 
 export default TaskTwo;
