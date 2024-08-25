e1eedcc2018a1c9067bc3cfbe30b679baff46f02
omarknows <omarknows98@gmail.com>
2024-08-25 17:56:24 +0300

    added container to task three

--- src/tasks/TaskThree.tsx
+++ src/tasks/TaskThree.tsx
@@ -1,7 +1,13 @@
-import Navbar from "./TaskThree/NavBar";
+import Container from '@components/Container';
+import Navbar from './TaskThree/NavBar';
 
 const TaskThree = () => {
-  return <Navbar />;
+	return (
+		<div>
+			<Navbar />
+			<Container title='Task Three: Build a Responsive Navigation Menu'></Container>
+		</div>
+	);
 };
 
 export default TaskThree;
